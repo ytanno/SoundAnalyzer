b4386faad44cc45521d81161dab0d419129f8b2d
tannoyoshinobu <tannoyoshinobu@gmail.com>
2016-01-06 13:19:19 +0900

    2016-01-06

---
+++ SoundForm/Form1.cs
@@ -0,0 +1,147 @@
+﻿//date 2016/01/04
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+// Nuget NAudio
+using NAudio.Wave;
+
+//ref http://wildpie.hatenablog.com/entry/2014/09/24/000900
+//ref http://stackoverflow.com/questions/3929389/naudio-convert-input-byte-array-to-an-array-of-doubles
+//ref http://pastie.org/10256216#14
+
+namespace SoundForm
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private WaveIn _wi = null;
+		private WaveFileWriter _writer = null;
+
+		//raw sound plot
+		PlotUtil _p1 = null;
+		private List<float> _recorded1 = new List<float>();
+
+		//processing sound plot
+		PlotUtil _p2 = null;
+		private List<float> _recorded2 = new List<float>();
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			InitSound();
+			InitPlot();
+		}
+
+		private void InitSound()
+		{
+			var deviceIndex = 0;
+
+			//Device Info
+			if (WaveIn.DeviceCount > 0)
+			{
+				var deviceInfo = WaveIn.GetCapabilities(0);
+				this.label1.Text = String.Format("Device {0}: {1}, {2} channels",
+					deviceIndex, deviceInfo.ProductName, deviceInfo.Channels);
+				_wi = new WaveIn() { DeviceNumber = deviceIndex };
+
+				//mike receive data
+				_wi.DataAvailable += (ss, ee) =>
+				{
+					for (int index = 0; index < ee.BytesRecorded; index += 2)
+					{
+						short sample = (short)((ee.Buffer[index + 1] << 8) | ee.Buffer[index + 0]);
+						float sample32 = 0;
+						if (sample > 0) sample32 = sample / 32767f;
+						if (sample < 0) sample32 = sample / 32768f;
+
+						_recorded1.Add(sample32);
+
+						if (_recorded1.Count == 1024)
+							_p1.UpdatePlot(_recorded1);
+
+						//Analyze Do here 
+						//ee.Buffer[index + 1] = AnalyzedData;
+						//ee.Buffer[index + 0] = AnalyzedData;
+
+					}
+					_writer.Write(ee.Buffer, 0, ee.BytesRecorded);
+					_writer.Flush();
+				};
+
+				_wi.WaveFormat = new WaveFormat(sampleRate: 8000, channels: deviceInfo.Channels);
+
+				//save file
+				var sPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop) + @"\file.wav";
+				_writer = new WaveFileWriter( sPath, _wi.WaveFormat);
+			}
+			else RecordButton.Enabled = false;
+		}
+
+		private void InitPlot()
+		{
+			this.SuspendLayout();
+
+			var size = new Size(400, 150);
+			var startPoint = new Point(10, 40);
+			var spaceHeight = 20;
+
+			_p1 = new PlotUtil(this, "p1", startPoint, size);
+			startPoint = new Point(startPoint.X, startPoint.Y + size.Height + spaceHeight);
+
+			_p2 = new PlotUtil(this, "p2", startPoint, size);
+		}
+
+		private void RecordButton_Click(object sender, EventArgs e)
+		{
+			if(RecordButton.Text == "Start")
+			{
+				//init data
+				_recorded1 = new List<float>();
+				_recorded2 = new List<float>();
+				_p1.UpdatePlot(_recorded1);
+				_p2.UpdatePlot(_recorded2);
+
+				RecordButton.Text = "Stop";
+				_wi.StartRecording();
+			}
+			else 
+			{
+				End();
+			}
+		}
+
+
+		private void End()
+		{
+			RecordButton.Enabled = false;
+			_wi.StopRecording();
+
+			if (_writer != null)
+			{
+				_writer.Dispose();
+				_writer = null;
+			}
+
+			if (_wi != null)
+			{
+				_wi.Dispose();
+				_wi = null;
+			}
+
+			InitSound();
+			RecordButton.Text = "Start";
+			RecordButton.Enabled = true;
+		}
+
+		
+
+
+	}
+}
---
+++ SoundForm/PlotUtil.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.Windows.Forms;
+
+//Install-Package OxyPlot.WindowsForms -Pre
+//I do not know how to add from tool box in windowsForm 
+using OxyPlot;
+using OxyPlot.Axes;
+using OxyPlot.Series;
+using OxyPlot.WindowsForms;
+
+namespace SoundForm
+{
+	public class PlotUtil
+	{
+		public PlotView _pv = null;
+		private LineSeries _lineSeries = null;
+
+		/// <summary>
+		/// Constructor
+		/// </summary>
+		/// <param name="setForm">Add WinForm</param>
+		/// <param name="name">Graph Name</param>
+		/// <param name="location">Graph Layout Start Point</param>
+		/// <param name="viewSize">Graph Size</param>
+		public PlotUtil(Form1 setForm, string name, Point location, Size viewSize)
+		{
+			_pv = new PlotView();
+			_lineSeries = new LineSeries();
+
+			PlotModel pm = new PlotModel();
+
+			LinearAxis lax1 = new LinearAxis { Position = AxisPosition.Bottom };
+			LinearAxis lax2 = new LinearAxis
+			{
+				Maximum = 1.0,
+				Minimum = -1.0,
+				Position = AxisPosition.Left
+			};
+
+			pm.Axes.Add(lax1);
+			pm.Axes.Add(lax2);
+			pm.Series.Add(_lineSeries);
+
+			_pv.Dock = DockStyle.Bottom;
+			_pv.Location = location;
+			_pv.Name = name;
+			_pv.PanCursor = Cursors.Hand;
+			_pv.Size = new Size(400, 150);
+			_pv.TabIndex = 0;
+			_pv.Text = name;
+			_pv.ZoomHorizontalCursor = Cursors.SizeWE;
+			_pv.ZoomRectangleCursor = Cursors.SizeNWSE;
+			_pv.ZoomVerticalCursor = Cursors.SizeNS;
+			_pv.Model = pm;
+
+			setForm.Controls.Add(_pv);
+		}
+
+		public void UpdatePlot(List<float> dataList)
+		{
+			var points = dataList.Select((v, index) =>
+						new DataPoint((double)index, v)
+					).ToList();
+			_lineSeries.Points.Clear();
+			_lineSeries.Points.AddRange(points);
+
+			_pv.InvalidatePlot(true);
+			dataList.Clear();
+		}
+
+	}
+}
